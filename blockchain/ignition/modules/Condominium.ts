e1140f005a6b84beb53ab4a438c91bbf1fd3b31c
Fernando Sávio Gomes <133050259+fernandosaviogo@users.noreply.github.com>
2026-01-16 09:57:52 -0300

    configuration for deployment

---
+++ blockchain/ignition/modules/Condominium.ts
@@ -0,0 +1,18 @@
+// This setup uses Hardhat Ignition to manage smart contract deployments.
+// Learn more about it at https://hardhat.org/ignition
+
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+
+const CondominiumModule = buildModule("CondominiumModule", (m) => {
+  const condominium = m.contract("Condominium")
+
+  const adapter = m.contract("CondominiumAdapter", [], {
+    after: [condominium],
+  });
+
+  m.call(adapter, "upgrade", [condominium]);
+
+  return { condominium, adapter };
+});
+
+export default CondominiumModule;
